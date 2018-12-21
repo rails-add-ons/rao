75f52d73c7a389ca6c8de8944e2d1fbbd9cf6d24
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-12-21 13:58:23 +0100

    Rao::Component: Add table_default_timestamp_format configuration option.

--- rao-component/lib/generators/rao/component/templates/initializer.rb
+++ rao-component/lib/generators/rao/component/templates/initializer.rb
@@ -1,2 +1,7 @@
 Rao::Component.configure do |config|
+  # Set the default format for timestamps in tables.
+  #
+  # Default: config.table_default_timestamp_format = nil
+  #
+  config.table_default_timestamp_format = nil
 end
--- rao-component/lib/rao/component/configuration.rb
+++ rao-component/lib/rao/component/configuration.rb
@@ -1,12 +1,11 @@
-require 'active_support/core_ext/module/delegation'
-require 'active_support/core_ext/module/attribute_accessors'
-
 module Rao
   module Component
     module Configuration
       def configure
         yield self
       end
+
+      mattr_accessor(:table_default_timestamp_format) { nil }
     end
   end
 end
