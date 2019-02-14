00e93ff478848847eef28749fbda46b8f3b2e020
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-02-14 09:53:51 +0100

    Rao::Component: Use correct namespace for partial.

--- rao-component/app/concerns/rao/component/collection_table/batch_actions_concern.rb
+++ rao-component/app/concerns/rao/component/collection_table/batch_actions_concern.rb
@@ -9,7 +9,7 @@ module Rao
       #
       def batch_actions(options = {}, &block)
         @wrap_in_form = true
-        title = @view.render partial: 'component/table/header_cells/batch_actions', locals: { options: options }
+        title = @view.render partial: 'rao/component/table/header_cells/batch_actions', locals: { options: options }
         options.reverse_merge!(render_as: :batch_actions, title: title)
         column(:batch_actions, options, &block)
       end
