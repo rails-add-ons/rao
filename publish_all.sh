987df34585880ec1491374a8e323369285e14574
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-02-21 10:58:28 +0100

    Rao: Add api-resources_controller to gem publish script.

--- publish_all.sh
+++ publish_all.sh
@@ -3,7 +3,7 @@ shopt -s expand_aliases
 source ~/.bash_aliases
 
 echo $PWD
-for i in api-service_controller component query resources_controller service service_controller view_helper; do
+for i in api-service_controller api-resources_controller component query resources_controller service service_controller view_helper; do
   cd "rao-${i}"
   echo $PWD
   publish_gem
