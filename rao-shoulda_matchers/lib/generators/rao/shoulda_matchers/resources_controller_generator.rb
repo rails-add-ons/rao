2dab646533f3b450ab956e5c7ce39e2feb88d7db
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-03-11 10:14:51 +0100

    Rao::ShouldaMatchers: Add resources controller spec generator.

---
+++ rao-shoulda_matchers/lib/generators/rao/shoulda_matchers/resources_controller_generator.rb
@@ -0,0 +1,55 @@
+module Rao
+  module ShouldaMatchers
+    # This will generate specs for create, read, update, delete and list.
+    # 
+    # Example:
+    # 
+    #     rails g rao:shoulda_matchers:resources_controller --uri /de/backend/uploads
+    #       create  spec/features/de/backend/uploads_feature_spec.rb
+    #
+    # If your resource class does not match the last part of your url (i.e.
+    # /de/posts would guess the resource class to Post) you can specify the resource
+    # name like this:
+    #
+    #     RESOURCE_CLASS=Blog::Post rails g rails:add_ons:resources_controller_spec --uri /de/posts
+    #       create  spec/features/de/posts_feature_spec.rb
+    #
+    class ResourcesControllerGenerator < Rails::Generators::Base
+      desc 'Generates CRUDL specs for REST resources'
+
+      source_root File.expand_path('../templates', __FILE__)
+ 
+      class_option :uri, type: :string, required: true
+      class_option :resource_class, type: :string
+
+      def uri
+        @uri ||= options['uri']
+      end
+
+      def edit_form_dom_selector
+        ".edit_#{resource_class.demodulize.underscore}"
+      end
+
+      def new_form_dom_selector
+        "#new_#{resource_class.demodulize.underscore}"
+      end
+
+      def resource_class
+        # @resource_class ||= ENV.fetch('RESOURCE_CLASS') { @uri.split('/').last.camelize.singularize }
+        @resource_class ||= options['resource_class'] ||= @uri.split('/').last.camelize.singularize
+      end
+
+      def factory_name
+        underscored_resource_class
+      end
+
+      def underscored_resource_class
+        @undescored_resource_class ||= resource_class.underscore.gsub('/', '_')
+      end
+
+      def generate_spec
+        template 'resources_controller_spec.rb', "spec/features#{uri}_feature_spec.rb"
+      end
+    end
+  end
+end
---
+++ rao-shoulda_matchers/lib/generators/rao/shoulda_matchers/templates/resources_controller_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe '<%= uri %>', type: :feature do
+  let(:resource_class) { <%= resource_class %> }
+  let(:resource) { create(:<%= factory_name %>) }
+  let(:resources) { create_list(:<%= factory_name %>, 3) }
+
+  # List
+  it { resources; expect(subject).to implement_index_action(self) }
+
+  # Create
+  it { 
+    expect(subject).to implement_create_action(self)
+      .for(resource_class)
+      .within_form('<%= new_form_dom_selector %>') {
+        # fill the needed form inputs via capybara here
+        #
+        # Example:
+        #
+        #     select 'de', from: 'slider[locale]'
+        #     fill_in 'slider[name]', with: 'My first slider'
+        #     check 'slider[auto_start]'
+        #     fill_in 'slider[interval]', with: '3'
+      }
+      .increasing{ <%= resource_class %>.count }.by(1)
+  }
+  
+  # Read
+  it { expect(subject).to implement_show_action(self).for(resource) }
+
+  # Update
+  it {
+    expect(subject).to implement_update_action(self)
+      .for(resource)
+      .within_form('<%= edit_form_dom_selector %>') {
+        # fill the needed form inputs via capybara here
+        # 
+        # Example:
+        # 
+        #     fill_in 'slider[name]', with: 'New name'
+      }
+      .updating
+      .from(resource.attributes)
+      .to({ }) # Example: .to({ 'name' => 'New name' })
+  }
+
+  # Delete
+  it {
+    expect(subject).to implement_delete_action(self)
+      .for(resource)
+      .reducing{ resource_class.count }.by(1)
+  }
+end
