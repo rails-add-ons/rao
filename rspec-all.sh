9e8ba2ae889efcfc3bfd14e511103c8176b9f2e1
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-03-28 23:33:04 +0100

    Bumped version to 0.0.16.pre

--- rspec-all.sh
+++ rspec-all.sh
@@ -5,6 +5,8 @@ bundle exec rspec spec
 for i in api-service_controller api-resources_controller component query resources_controller service service_controller shoulda_matchers view_helper; do
   cd "rao-${i}"
   echo $PWD
+  rm Gemfile.lock
+  bundle
   bundle exec rspec spec
   cd ..
 done
