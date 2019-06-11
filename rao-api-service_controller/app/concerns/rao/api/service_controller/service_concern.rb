9025a15b9f45d480b848f788c9175e45b5a5d09a
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-06-11 17:32:05 +0200

    Rao::Api::ServiceController: Remove service_class helper method for ActionController::API compatibility.

--- rao-api-service_controller/app/concerns/rao/api/service_controller/service_concern.rb
+++ rao-api-service_controller/app/concerns/rao/api/service_controller/service_concern.rb
@@ -3,10 +3,6 @@ module Rao
     module ServiceController::ServiceConcern
       extend ActiveSupport::Concern
 
-      included do
-        helper_method :service_class
-      end
-
       def service_class
         unless self.class.respond_to?(:service_class)
           raise "undefined method `service_class' for #{self.class.name}: Add a service_class method to your controller. Example: def self.service_class; MyAmazingService; end"
