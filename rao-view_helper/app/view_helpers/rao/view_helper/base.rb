2b61a01ebf68891caac40951c4d0ee34215f871e
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-11-25 11:58:01 +0100

    Rao::ViewHelper: Make view helpers i18n aware.

--- rao-view_helper/app/view_helpers/rao/view_helper/base.rb
+++ rao-view_helper/app/view_helpers/rao/view_helper/base.rb
@@ -32,6 +32,33 @@ module Rao
       def render(locals = {})
         c.render partial: "/#{self.class.name.underscore}/#{caller_locations(1,1)[0].label}", locals: locals
       end
+
+      # You can use scoped translations by using the dot notation.
+      #
+      # Example:
+      #
+      #     # app/view_helpers/blog_view_helper.rb
+      #     class BlogViewHelper < Rao::ViewHelper::Base
+      #       def title
+      #         t('.title')
+      #       end
+      #     end
+      #
+      #     # config/locales/en.yml
+      #     en:
+      #       blog_view_helper:
+      #         title: 'Posts'
+      #
+      #     blog_view_helper(self).title
+      #       => "Posts"
+      #
+      module I18nConcern
+        def t(key, options = {})
+          I18n.t("#{self.class.name.underscore}#{key}", options)
+        end
+      end
+
+      include I18nConcern
     end
   end
 end
---
+++ rao-view_helper/spec/view_helpers/rao/view_helper/base/i18n_concern_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+I18n.backend.store_translations("en",
+  {
+    view_helper_with_i18n_concern: {
+      foo: "Foo (en)"
+    }
+  }
+)
+
+class ViewHelperWithI18nConcern < Rao::ViewHelper::Base
+  def render_translated
+    t('.foo')
+  end
+end
+
+RSpec.describe ViewHelperWithI18nConcern, type: :view_helper do
+  describe 'I18nConcern' do
+    describe 'render_translated' do
+      it { expect(rendered).to eq('Foo (en)') }
+    end
+  end
+end
