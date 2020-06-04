94f42eb44b0cbcdd2586a411c83f24cc306a3380
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2020-06-04 13:03:36 +0200

    Rao::Component: Add aasm support to the collection table. Rao::Component: Add active storage support to the collection table. Rao::ResourcesController: Add aasm concern.

---
+++ rao-resources_controller/app/concerns/rao/resources_controller/aasm_concern.rb
@@ -0,0 +1,30 @@
+module Rao
+  module ResourcesController
+    module AasmConcern
+      extend ActiveSupport::Concern
+
+      included do
+        before_action :load_resource_for_trigger_event, only: [:trigger_event]
+      end
+
+      def trigger_event
+        if @resource.aasm(permitted_params_for_trigger_event[:machine_name].to_sym).fire!(permitted_params_for_trigger_event[:event_name].to_sym)
+          flash[:notice] = t('controller.aasm_concern.trigger_event.success', state: @resource.aasm(permitted_params_for_trigger_event[:machine_name].to_sym).current_state, event: permitted_params_for_trigger_event[:event_name])
+        else
+          flash[:notice] = t('controller.aasm_concern.trigger_event.failure', state: @resource.aasm(permitted_params_for_trigger_event[:machine_name].to_sym).current_state, event: permitted_params_for_trigger_event[:event_name])
+        end
+        redirect_back(fallback_location: root_path)
+      end
+
+      private
+
+      def permitted_params_for_trigger_event
+        params.permit!
+      end
+
+      def load_resource_for_trigger_event
+        load_resource
+      end
+    end
+  end
+end
