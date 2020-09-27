4ab8967eb6675f4d3c04843944479b7b3a070df0
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2020-09-27 03:11:33 +0200

    Rao::ResourcesController: Add missing translations. Rao::ResourcesController: Add label_for_resource_proc configuration option. Rao::ResourcesController: Fix views depending on obsolete methods.

--- rao-resources_controller/lib/generators/rao/resources_controller/templates/initializer.rb
+++ rao-resources_controller/lib/generators/rao/resources_controller/templates/initializer.rb
@@ -12,4 +12,20 @@ Rao::ResourcesController.configure do |config|
   # default: config.pagination_per_page_default = 25
   #
   config.pagination_per_page_default = 25
+
+  # Configures how a label will be created for resources.
+  #
+  # default: config.label_for_resource_proc = lambda do |resource|
+  #            [:human, :title, :name, :to_s].each do |method_name|
+  #              next unless resource.respond_to?(method_name)
+  #              return resource.send(method_name)
+  #            end
+  #          end
+  #
+  config.label_for_resource_proc = lambda do |resource|
+    [:human, :title, :name, :to_s].each do |method_name|
+      next unless resource.respond_to?(method_name)
+      return resource.send(method_name)
+    end
+  end
 end
--- rao-resources_controller/lib/rao/resources_controller/configuration.rb
+++ rao-resources_controller/lib/rao/resources_controller/configuration.rb
@@ -5,8 +5,22 @@ module Rao
         yield self
       end
 
-      mattr_accessor(:resources_controller_base_class_name) { "::ApplicationController" }
-      mattr_accessor(:pagination_per_page_default) { 25 }
+      mattr_accessor(:resources_controller_base_class_name) do
+        "::ApplicationController"
+      end
+
+      mattr_accessor(:pagination_per_page_default) do
+        25
+      end
+
+      mattr_accessor(:label_for_resource_proc) do
+        lambda do |resource|
+          [:human, :title, :name, :to_s].each do |method_name|
+            next unless resource.respond_to?(method_name)
+            return resource.send(method_name)
+          end
+        end
+      end
     end
   end
 end
