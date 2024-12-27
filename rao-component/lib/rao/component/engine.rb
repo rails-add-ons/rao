8980dca8b67a6bb4a8fc5300a640840eae7aa3a5
Roberto Vasquez Angel <rva@beegoodit.de>
2024-12-27 13:06:22 +0100

    fix(Rao::Component): typo fix(Rao::Component): assets with propshaft fix(Rao::Component): routing for aasm event triggering

--- rao-component/lib/rao/component/engine.rb
+++ rao-component/lib/rao/component/engine.rb
@@ -2,10 +2,6 @@ module Rao
   module Component
     class Engine < ::Rails::Engine
       isolate_namespace Rao::Component
-      
-      initializer "rao.component.assets" do |app|
-        app.config.assets.paths << root.join("app/assets")
-      end
     end
   end
 end
