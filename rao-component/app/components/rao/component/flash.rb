a86532d43a0d72d698dc2b4fa786870f7e501aad
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2025-09-23 12:23:24 +0200

    feat(Rao:Component): enhance flash message handling with mapped context for bootstrap compatibility

--- rao-component/app/components/rao/component/flash.rb
+++ rao-component/app/components/rao/component/flash.rb
@@ -6,10 +6,26 @@ module Rao
         @context = context
         @options = options
         @flash = @context.flash
+        @mapped_flash = mapped_flash
+        @view_locals = {
+          mapped_flash: mapped_flash
+        }
       end
 
       def render
         perform
+      end
+
+      private
+
+      def mapped_flash
+        @context.flash.each_with_object({}) do |(key, value), memo|
+          memo[map_message_type_to_context(key)] = value
+        end
+      end
+
+      def map_message_type_to_context(message_type)
+        {success: "success", error: "danger", alert: "warning", notice: "info"}[message_type.to_sym] || message_type.to_s
       end
     end
   end
