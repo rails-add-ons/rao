add990dcb74eb7784b138528757e6ca68ad51fa0
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2025-09-29 08:51:08 +0200

    chore(Rao:Component): improve dummy initialization script

--- rao-resources_controller/initialize_dummy.sh
+++ rao-resources_controller/initialize_dummy.sh
@@ -63,6 +63,9 @@ sed -i '2i\  root to: "home#index"' config/routes.rb
 # Generate models
 rails g model post title body:text published_at:timestamp
 rails g model user name email bio
+rm -rf spec/helpers
+rm -rf spec/models
+rm -rf spec/views
 
 # Add route
 sed -i '2i\  resources :posts\n  resource :user\n  resolve("User") { [:user] }' config/routes.rb 
@@ -73,51 +76,8 @@ sed -i '2i\  helper Rao::Component::FlashHelper' app/controllers/application_con
 # render flash in application layout before <%= yield %>
 sed -i '/<%= yield %>/i\    <%= flash_messages.render %>' app/views/layouts/application.html.erb
 
-# Create controllers
-mkdir -p app/controllers
-cat > app/controllers/posts_controller.rb << 'EOF'
-class PostsController < ApplicationController
-  include Rao::ResourcesController::ResourceInflectionsConcern
-  include Rao::ResourcesController::DefaultViewsConcern
-  include Rao::ResourcesController::Plural::RestResourcesUrlsConcern
-  include Rao::ResourcesController::Plural::ResourcesConcern
-  include Rao::ResourcesController::Plural::RestActionsConcern
-
-  def self.resource_class
-    Post
-  end
-
-  private
-
-  def resource_params
-    params.require(:post).permit(:title, :body)
-  end
-end
-EOF
-
-cat > app/controllers/users_controller.rb << 'EOF'
-class UsersController < ApplicationController
-  include Rao::ResourcesController::ResourceInflectionsConcern
-  include Rao::ResourcesController::DefaultViewsConcern
-  include Rao::ResourcesController::Singular::RestResourcesUrlsConcern
-  include Rao::ResourcesController::Singular::ResourcesConcern
-  include Rao::ResourcesController::Singular::RestActionsConcern
-
-  def self.resource_class
-    User
-  end
-
-  private
-
-  def load_resource
-    @resource = User.first
-  end
-
-  def resource_params
-    params.require(:user).permit(:name, :email, :bio)
-  end
-end
-EOF
+# Copy all files from spec/setup to dummy app
+cp -r $CURRENT_DIR/spec/setup/. .
 
 # Setup database
 rails db:migrate db:test:prepare
